b2b9fce9f7ceb99a1b150c46b3bfbbe84757d5a1
tatiane <tati.guisso@gmail.com>
2024-02-20 19:34:15 -0300

    first commit

---
+++ src/test/java/com/grupo16/produtoservice/ProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo16.produtoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
