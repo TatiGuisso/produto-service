63012400ad3798e95bfa6e55fa2e113c519605a7
bruno-gds <brunog.damascena@gmail.com>
2024-02-21 23:46:07 -0300

    iniciando ProdutoController

---
+++ src/main/java/com/grupo16/produtoservice/gateway/controller/ProdutoController.java
@@ -0,0 +1,19 @@
+package com.grupo16.produtoservice.gateway.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 21/02/2024
+ * Project Name: produto-service
+ */
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("produtos")
+public class ProdutoController {
+}
