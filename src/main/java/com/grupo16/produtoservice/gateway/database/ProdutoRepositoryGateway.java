f208eb4bde82184354ffcdc980823b7e983d5757
bruno-gds <brunog.damascena@gmail.com>
2024-02-21 23:45:24 -0300

    iniciando ProdutoRepositoryGateway

---
+++ src/main/java/com/grupo16/produtoservice/gateway/database/ProdutoRepositoryGateway.java
@@ -0,0 +1,10 @@
+package com.grupo16.produtoservice.gateway.database;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 21/02/2024
+ * Project Name: produto-service
+ */
+
+public interface ProdutoRepositoryGateway {
+}
