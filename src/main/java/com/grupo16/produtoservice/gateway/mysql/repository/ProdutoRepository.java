b7991b38009bc0806f286da61ded772da4af103e
bruno-gds <brunog.damascena@gmail.com>
2024-02-21 23:20:23 -0300

    adicionando repository

---
+++ src/main/java/com/grupo16/produtoservice/gateway/mysql/repository/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package com.grupo16.produtoservice.gateway.mysql.repository;
+
+import com.grupo16.produtoservice.gateway.mysql.entity.ProdutoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 21/02/2024
+ * Project Name: produto-service
+ */
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long> {
+}
