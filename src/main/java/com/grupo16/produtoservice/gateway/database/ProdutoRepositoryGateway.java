fbe460cda8082474252a59b096346de03466e1c9
bruno-gds <brunog.damascena@gmail.com>
2024-02-22 22:27:20 -0300

    adicionando salvar na ProdutoRepositoryGateway

--- src/main/java/com/grupo16/produtoservice/gateway/database/ProdutoRepositoryGateway.java
+++ src/main/java/com/grupo16/produtoservice/gateway/database/ProdutoRepositoryGateway.java
@@ -1,5 +1,7 @@
 package com.grupo16.produtoservice.gateway.database;
 
+import com.grupo16.produtoservice.domain.Produto;
+
 /**
  * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
  * Date: 21/02/2024
@@ -7,4 +9,6 @@ package com.grupo16.produtoservice.gateway.database;
  */
 
 public interface ProdutoRepositoryGateway {
+
+    Long salvar(Produto produto);
 }
