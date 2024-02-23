57db085f639cbef6685e8ac623bae9d9bc3e78fd
bruno-gds <brunog.damascena@gmail.com>
2024-02-23 20:05:28 -0300

    implementando ProdutoNaoEncontradoException

---
+++ src/main/java/com/grupo16/produtoservice/service/exception/ProdutoNaoEncontradoException.java
@@ -0,0 +1,19 @@
+package com.grupo16.produtoservice.service.exception;
+
+import com.grupo16.produtoservice.exception.SystemBaseException;
+import lombok.Getter;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 23/02/2024
+ * Project Name: produto-service
+ */
+
+@Getter
+public class ProdutoNaoEncontradoException extends SystemBaseException {
+    private static final long serialVersionUID = 5199898008763121907L;
+
+    private final String code = "tc.produtoNaoEncontrado";
+    private final String message = "Produto não encontrado.";
+    private final Integer httpStatus = 404;
+}
