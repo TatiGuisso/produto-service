31b8d0a7de5c617f66f981aa66214b761f82383b
bruno-gds <brunog.damascena@gmail.com>
2024-02-29 20:46:57 -0300

    implementando alterar na CriarAlterarProdutoService

--- src/main/java/com/grupo16/produtoservice/service/CriarAlterarProdutoService.java
+++ src/main/java/com/grupo16/produtoservice/service/CriarAlterarProdutoService.java
@@ -2,8 +2,12 @@ package com.grupo16.produtoservice.service;
 
 import com.grupo16.produtoservice.domain.Produto;
 import com.grupo16.produtoservice.gateway.database.ProdutoRepositoryGateway;
+import com.grupo16.produtoservice.service.exception.ProdutoNaoEncontradoException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
@@ -13,11 +17,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CriarAlterarProdutoService {
 
     private final ProdutoRepositoryGateway produtoRepositoryGateway;
 
     public Long criar(Produto produto) {
         return this.produtoRepositoryGateway.salvar(produto);
+    }
+
+    public void alterar(Long id, Produto produto) {
+        Optional<Produto> produtoOp = this.produtoRepositoryGateway.obterPorId(id);
+
+        if (produtoOp.isEmpty()) {
+            log.warn("Produto não encontrado: id={}", id);
+            throw new ProdutoNaoEncontradoException();
+        }
+
+        Produto produtoSalvo = Produto.builder()
+                .id(id)
+                .nome(produto.getNome())
+                .descricao(produto.getDescricao())
+                .preco(produto.getPreco())
+                .status(produto.getStatus())
+                .dataCriacao(produto.getDataCriacao())
+                .build();
+
+        this.produtoRepositoryGateway.salvar(produtoSalvo);
     }
 }
